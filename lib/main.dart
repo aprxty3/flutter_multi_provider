5756357e52efc198e0422051430503eccf01c3e0
aprxrt3 <ajiprxty3@gmail.com>
2021-11-21 21:01:42 +0700

    add provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_multi_provider/homepage.dart';
+import 'package:flutter_multi_provider/provider/hei_provider.dart';
+import 'package:flutter_multi_provider/provider/wei_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      debugShowCheckedModeBanner: false,
-      home: homePage(),
+    return ChangeNotifierProvider<weightProvider>(
+      create: (context) => weightProvider(),
+      child: ChangeNotifierProvider<heightProvider>(
+        create: (context) => heightProvider(),
+        child: MaterialApp(
+          title: 'Material App',
+          debugShowCheckedModeBanner: false,
+          home: homePage(),
+        ),
+      ),
     );
   }
 }
